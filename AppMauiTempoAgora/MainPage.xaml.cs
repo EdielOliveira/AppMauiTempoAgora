8b51a918d3a74ce0dad718e7c030d3ae11deb429
EdielOliveira <ediel.inacio@outlook.com>
2025-04-26 20:30:52 -0300

    Adicionando Api

--- AppMauiTempoAgora/MainPage.xaml.cs
+++ AppMauiTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace AppMauiTempoAgora
+﻿using AppMauiTempoAgora.Models;
+using AppMauiTempoAgora.Services;
+
+namespace AppMauiTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -8,17 +11,44 @@
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Maxima: {t.temp_max} \n" +
+                                         $"Temp Minima: {t.temp_min} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do Vento: {t.speed} m/s \n" +
+                                         $"Visibilidade: {t.visibility} metros";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsao";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "Continuar");
+            }
         }
     }
 
---
+++ AppMauiTempoAgora/Services/DataService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using AppMauiTempoAgora.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+using System.Net.Http;
+
+namespace AppMauiTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "02307b4d1a558723bcdaca3c2aec4c3f";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                        $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+
+                        var rascunnho = JObject.Parse(json);
+
+                        DateTime time = new();
+                        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds((long)rascunnho["sys"]["sunrise"]);
+                        DateTime sunrise = dateTimeOffset.LocalDateTime;
+
+
+                        dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds((long)rascunnho["sys"]["sunset"]);
+                        DateTime sunset = dateTimeOffset.LocalDateTime;
+
+
+                        t = new()
+                        {
+                            lat = (double)rascunnho["coord"]["lat"],
+                            lon = (double)rascunnho["coord"]["lon"],
+                            description = (string)rascunnho["weather"][0]["description"],
+                            main = (string)rascunnho["weather"][0]["main"],
+                            temp_min = (double)rascunnho["main"]["temp_min"],
+                            temp_max = (double)rascunnho["main"]["temp_max"],
+                            speed = (double)rascunnho["wind"]["speed"],
+                            visibility = (int)rascunnho["visibility"],
+                            sunrise = sunrise.ToString(),
+                            sunset = sunset.ToString(),
+                        };
+                    }
+                    else if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        throw new Exception("Cidade não encontrada. Verifique o nome e tente novamente.");
+                    }
+                    else
+                    {
+                        throw new Exception("Erro ao buscar a previsão do tempo. Tente novamente mais tarde.");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw new Exception("Sem conexão com a internet. Verifique sua rede e tente novamente.");
+                }
+            }
+
+            return t;
+        }
+    }
+}
